f2c957a727c8c74e93270d91f3af01d28dfe8f7d
cheichey <cheichey@users.noreply.github.com>
2021-04-21 19:33:42 +0000

    deploy: 56fec5da6c07d1e183a87cba7ea5f7246711fb85

--- main.js
+++ main.js
@@ -56950,6 +56950,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "URL": function() { return /* binding */ URL; },
 /* harmony export */   "getToken": function() { return /* binding */ getToken; },
 /* harmony export */   "login": function() { return /* binding */ login; },
+/* harmony export */   "getAuth": function() { return /* binding */ getAuth; },
 /* harmony export */   "logout": function() { return /* binding */ logout; },
 /* harmony export */   "getFile": function() { return /* binding */ getFile; },
 /* harmony export */   "getDocument": function() { return /* binding */ getDocument; },
@@ -56973,6 +56974,7 @@ var login = function (id, pass) {
     };
     return axios__WEBPACK_IMPORTED_MODULE_1___default().post(URL + "/auth", null, { headers: header });
 };
+var getAuth = function (cookie) { return axios__WEBPACK_IMPORTED_MODULE_1___default().get(URL + "/auth", { headers: { cookie: cookie } }); };
 var logout = function () { return axios__WEBPACK_IMPORTED_MODULE_1___default().post(URL + "/logout"); };
 var getFile = function (path) { return axios__WEBPACK_IMPORTED_MODULE_1___default().get(URL + "/file/" + path); };
 var getDocument = function (documentName) { return axios__WEBPACK_IMPORTED_MODULE_1___default().get(URL + "/document/" + documentName); };
@@ -57153,11 +57155,12 @@ var Account = function () {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
-/* harmony import */ var _material_ui_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @material-ui/core */ "./node_modules/@material-ui/core/esm/CircularProgress/CircularProgress.js");
-/* harmony import */ var _material_ui_core__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @material-ui/core */ "./node_modules/@material-ui/core/esm/TextField/TextField.js");
-/* harmony import */ var _material_ui_core__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @material-ui/core */ "./node_modules/@material-ui/core/esm/Button/Button.js");
-/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router/esm/react-router.js");
-/* harmony import */ var _contexts_UserContext__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../contexts/UserContext */ "./src/contexts/UserContext.tsx");
+/* harmony import */ var _material_ui_core__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @material-ui/core */ "./node_modules/@material-ui/core/esm/CircularProgress/CircularProgress.js");
+/* harmony import */ var _material_ui_core__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @material-ui/core */ "./node_modules/@material-ui/core/esm/TextField/TextField.js");
+/* harmony import */ var _material_ui_core__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @material-ui/core */ "./node_modules/@material-ui/core/esm/Button/Button.js");
+/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router/esm/react-router.js");
+/* harmony import */ var _api__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../api */ "./src/api.ts");
+/* harmony import */ var _contexts_UserContext__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../contexts/UserContext */ "./src/contexts/UserContext.tsx");
 var __assign = (undefined && undefined.__assign) || function () {
     __assign = Object.assign || function(t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
@@ -57169,13 +57172,50 @@ var __assign = (undefined && undefined.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
+var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (undefined && undefined.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+
 
 
 
 
 var Login = function () {
     var _a, _b, _c, _d, _e, _f;
-    var auth = (0,_contexts_UserContext__WEBPACK_IMPORTED_MODULE_1__.useAuth)();
+    var auth = (0,_contexts_UserContext__WEBPACK_IMPORTED_MODULE_2__.useAuth)();
     var handleIdForm = function (event) {
         console.log(event.target.value);
         auth.setUser(__assign(__assign({}, auth.user), { inputId: event.target.value }));
@@ -57195,19 +57235,44 @@ var Login = function () {
         auth.getToken();
     };
     console.log((_a = auth.user) === null || _a === void 0 ? void 0 : _a.isLoading, (_b = auth.user) === null || _b === void 0 ? void 0 : _b.postedId);
-    if (((_c = auth.user) === null || _c === void 0 ? void 0 : _c.isLoading) === true) {
-        return ((0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_material_ui_core__WEBPACK_IMPORTED_MODULE_2__.default, {}, void 0));
+    var isLoggedIn = ((_c = auth.user) === null || _c === void 0 ? void 0 : _c.isLoggedIn) === true;
+    if (!isLoggedIn) {
+        var cookie_1 = document.cookie.split(';').find(function (line) { return line.startsWith('auth='); });
+        if (cookie_1) {
+            var checkSession = function () { return __awaiter(void 0, void 0, void 0, function () {
+                var result;
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0: return [4 /*yield*/, _api__WEBPACK_IMPORTED_MODULE_1__.getAuth(cookie_1)];
+                        case 1:
+                            result = _a.sent();
+                            if (result.status === 200) {
+                                auth.setUser(__assign(__assign({}, auth.user), { isLoggedIn: true }));
+                                isLoggedIn = true;
+                            }
+                            else if (result.status === 401) {
+                                document.cookie = 'auth=; max-age=0';
+                            }
+                            return [2 /*return*/];
+                    }
+                });
+            }); };
+            checkSession();
+        }
     }
-    if (((_d = auth.user) === null || _d === void 0 ? void 0 : _d.postedId) === undefined) {
-        return ((0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("form", __assign({ className: "login", onSubmit: handleIdFormSubmit }, { children: [(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_material_ui_core__WEBPACK_IMPORTED_MODULE_3__.default, { type: "text", onChange: handleIdForm }, void 0),
-                (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_material_ui_core__WEBPACK_IMPORTED_MODULE_4__.default, __assign({ onClick: handleIdFormSubmit, variant: "contained", color: "primary" }, { children: "\u30E1\u30FC\u30EB\u3092\u9001\u308B" }), void 0)] }), void 0));
+    if (((_d = auth.user) === null || _d === void 0 ? void 0 : _d.isLoading) === true) {
+        return ((0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_material_ui_core__WEBPACK_IMPORTED_MODULE_3__.default, {}, void 0));
     }
-    if (((_e = auth.user) === null || _e === void 0 ? void 0 : _e.isLoggedIn) === true) {
-        return ((0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(react_router_dom__WEBPACK_IMPORTED_MODULE_5__.Redirect, { to: "/Account" }, void 0));
+    if (((_e = auth.user) === null || _e === void 0 ? void 0 : _e.postedId) === undefined) {
+        return ((0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("form", __assign({ className: "login", onSubmit: handleIdFormSubmit }, { children: [(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_material_ui_core__WEBPACK_IMPORTED_MODULE_4__.default, { type: "text", onChange: handleIdForm }, void 0),
+                (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_material_ui_core__WEBPACK_IMPORTED_MODULE_5__.default, __assign({ onClick: handleIdFormSubmit, variant: "contained", color: "primary" }, { children: "\u30E1\u30FC\u30EB\u3092\u9001\u308B" }), void 0)] }), void 0));
     }
-    return ((0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("form", __assign({ className: "login" }, { children: [(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_material_ui_core__WEBPACK_IMPORTED_MODULE_3__.default, { type: "text", onChange: handleIdForm, defaultValue: (_f = auth.user) === null || _f === void 0 ? void 0 : _f.inputId }, void 0),
-            (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_material_ui_core__WEBPACK_IMPORTED_MODULE_3__.default, { type: "text", onChange: handlePassWordForm }, void 0),
-            (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_material_ui_core__WEBPACK_IMPORTED_MODULE_4__.default, __assign({ onClick: handleLoginFormSubmit, variant: "contained", color: "primary" }, { children: "\u30ED\u30B0\u30A4\u30F3\u3059\u308B" }), void 0)] }), void 0));
+    if (isLoggedIn) {
+        return ((0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(react_router_dom__WEBPACK_IMPORTED_MODULE_6__.Redirect, { to: "/Account" }, void 0));
+    }
+    return ((0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("form", __assign({ className: "login" }, { children: [(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_material_ui_core__WEBPACK_IMPORTED_MODULE_4__.default, { type: "text", onChange: handleIdForm, defaultValue: (_f = auth.user) === null || _f === void 0 ? void 0 : _f.inputId }, void 0),
+            (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_material_ui_core__WEBPACK_IMPORTED_MODULE_4__.default, { type: "text", onChange: handlePassWordForm }, void 0),
+            (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_material_ui_core__WEBPACK_IMPORTED_MODULE_5__.default, __assign({ onClick: handleLoginFormSubmit, variant: "contained", color: "primary" }, { children: "\u30ED\u30B0\u30A4\u30F3\u3059\u308B" }), void 0)] }), void 0));
 };
 /* harmony default export */ __webpack_exports__["default"] = (Login);
 
@@ -57267,6 +57332,7 @@ var useAuthCtx = function () {
         var result = _api__WEBPACK_IMPORTED_MODULE_2__.login(user === null || user === void 0 ? void 0 : user.inputId, user === null || user === void 0 ? void 0 : user.inputPassWord)
             .then(function (res) {
             setUser(__assign(__assign({}, user), { isLoading: false, isLoggedIn: true }));
+            document.cookie = "auth=" + res.data.auth;
             console.log(res);
         })
             .catch(function (err) { console.log(err); });
