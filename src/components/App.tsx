821b00d3cd1ce91af5e2bf709c6a1f161f429fea
butsuli-shine <cheichey.1223@gmail.com>
2021-04-21 18:31:34 +0900

    feat: useContext を使ったグローバルな状態管理システムを追加した

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,21 @@
-import React, { FC } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import { AuthProvider } from '../contexts/UserContext';
 import './App.scss';
 
-const App: FC = (): JSX.Element => (
-  <div className="App">
-    <p>Hello World</p>
-  </div>
+const App = (): JSX.Element => (
+  <AuthProvider>
+    <div className="app">
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/login" component={Login} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  </AuthProvider>
 );
 
 export default App;
