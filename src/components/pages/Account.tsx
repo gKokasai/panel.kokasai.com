46f90bc7cb1d8f186f3e841e3f28af4eaed3cf43
butsuli-shine <cheichey.1223@gmail.com>
2021-04-22 16:02:10 +0900

    feat: 未実装の旨を追記した

--- src/components/pages/Account.tsx
+++ src/components/pages/Account.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
-import { useAuth } from '../../contexts/UserContext';
-import * as api from '../../api';
+import React /* { useEffect } */　from 'react';
+/* import { useAuth } from '../../contexts/UserContext';
+import * as api from '../../api'; */
 
-const Account = (): JSX.Element => {
-  const auth = useAuth();
+const Account = (): JSX.Element =>
+/*  const auth = useAuth();
   useEffect(
     () => {
       const asyncGet = async () => {
@@ -13,17 +13,20 @@ const Account = (): JSX.Element => {
       };
       asyncGet().then().catch();
     }, [],
-  );
-
-  return (
-    <div>
+  ); */
+  (
+    <div className="gomi">
+      これ以降のページは未実装です。
+      来週から企画案の提出開始予定ですので、それまでに開発します！！！！！！！
+    </div>
+  /*
+      <div>
       {auth.user?.documentList?.map(
         (name) => (
           <li><a href={`${api.URL}/document/${name}`}>{name}</a></li>
         ),
       )}
     </div>
+*/
   );
-};
-
 export default Account;
