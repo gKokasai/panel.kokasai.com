82fc891171fcfbb4db46c7c6ddc9aac55d77d37f
sya-ri <sya-ri@users.noreply.github.com>
2021-04-22 07:03:59 +0000

    deploy: 3d1db9745a276eb05d28272de77862fd9d16ac66

--- main.js
+++ main.js
@@ -58180,9 +58180,6 @@ var LoginForm = function (props) {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
-/* harmony import */ var _contexts_UserContext__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../contexts/UserContext */ "./src/contexts/UserContext.tsx");
-/* harmony import */ var _api__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../api */ "./src/api.ts");
 var __assign = (undefined && undefined.__assign) || function () {
     __assign = Object.assign || function(t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
@@ -58194,66 +58191,32 @@ var __assign = (undefined && undefined.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
-var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (undefined && undefined.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 
-
-
-
+/* import { useAuth } from '../../contexts/UserContext';
+import * as api from '../../api'; */
 var Account = function () {
-    var _a, _b;
-    var auth = (0,_contexts_UserContext__WEBPACK_IMPORTED_MODULE_2__.useAuth)();
-    (0,react__WEBPACK_IMPORTED_MODULE_1__.useEffect)(function () {
-        var asyncGet = function () { return __awaiter(void 0, void 0, void 0, function () {
-            var result;
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0: return [4 /*yield*/, _api__WEBPACK_IMPORTED_MODULE_3__.getAccessibleDocumentList()];
-                    case 1:
-                        result = _a.sent();
-                        console.log(result);
-                        auth.setUser(__assign(__assign({}, auth.user), { documentList: result.data.document }));
-                        return [2 /*return*/];
-                }
-            });
-        }); };
-        asyncGet().then().catch();
-    }, []);
-    return ((0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", { children: (_b = (_a = auth.user) === null || _a === void 0 ? void 0 : _a.documentList) === null || _b === void 0 ? void 0 : _b.map(function (name) { return ((0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("li", { children: (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("a", __assign({ href: _api__WEBPACK_IMPORTED_MODULE_3__.URL + "/document/" + name }, { children: name }), void 0) }, void 0)); }) }, void 0));
+    /*  const auth = useAuth();
+      useEffect(
+        () => {
+          const asyncGet = async () => {
+            const result = await api.getAccessibleDocumentList();
+            console.log(result);
+            auth.setUser({ ...auth.user, documentList: result.data.document });
+          };
+          asyncGet().then().catch();
+        }, [],
+      ); */
+    return ((0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", __assign({ className: "gomi" }, { children: "\u3053\u308C\u4EE5\u964D\u306E\u30DA\u30FC\u30B8\u306F\u672A\u5B9F\u88C5\u3067\u3059\u3002 \u6765\u9031\u304B\u3089\u4F01\u753B\u6848\u306E\u63D0\u51FA\u958B\u59CB\u4E88\u5B9A\u3067\u3059\u306E\u3067\u3001\u305D\u308C\u307E\u3067\u306B\u958B\u767A\u3057\u307E\u3059\uFF01\uFF01\uFF01\uFF01\uFF01\uFF01\uFF01" }), void 0)
+    /*
+        <div>
+        {auth.user?.documentList?.map(
+          (name) => (
+            <li><a href={`${api.URL}/document/${name}`}>{name}</a></li>
+          ),
+        )}
+      </div>
+  */
+    );
 };
 /* harmony default export */ __webpack_exports__["default"] = (Account);
 
