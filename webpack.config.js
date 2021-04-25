ac460eb8b8fdc6a73cb0ceaad867e62e504978b9
sya-ri <sya79lua@gmail.com>
2021-04-25 12:57:18 +0900

    chore: ローカルIPをドメインに割り当てた panel -> local-panel.kokasai.com api -> local-api.kokasai.com

--- webpack.config.js
+++ webpack.config.js
@@ -54,6 +54,8 @@ module.exports = {
         })
     ],
     devServer: {
-        historyApiFallback: true
+        historyApiFallback: true,
+        host: 'local-panel.kokasai.com',
+        port: 8080
     }
 };
