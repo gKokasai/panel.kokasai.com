4eac97732299fc6c97a74e70a5e7f598f7e3841f
butsuli-shine <cheichey.1223@gmail.com>
2021-05-07 09:50:25 +0900

    fix: loadingのスタイルの改善をした

--- src/components/organisms/LoginForm.style.tsx
+++ src/components/organisms/LoginForm.style.tsx
@@ -18,11 +18,28 @@ const LoginFormStyle = makeStyles((theme: Theme) => createStyles({
   },
   card: {
     marginTop: theme.spacing(10),
+    maxWidth: '500px',
+    minHeight: '230px',
   },
   cardActions: {
     display: 'flex',
     flexWrap: 'wrap',
   },
+  cardContent: {
+    position: 'relative',
+    height: '200px',
+  },
+  circularProgress: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    margin: 'auto',
+    width: '50px',
+    height: '50px',
+
+  }
 }));
 
 export default LoginFormStyle;
