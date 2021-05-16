0bf968fbe01f762e77024572df6884a7974b69fd
sya-ri <sya79lua@gmail.com>
2021-05-16 14:12:13 +0900

    perf: APIコールをリロード時のみ行うように変更

--- src/components/pages/Document.tsx
+++ src/components/pages/Document.tsx
@@ -1,27 +1,12 @@
-import React, { FC, useEffect } from 'react';
-import { useAuth } from '../../contexts/AuthContext';
+import React, { FC } from 'react';
 import ControlPanelTemplate from '../templates/ControlPanelTemplate';
-import { getUserDocumentList } from '../../api/api';
 import { Pages } from '../../pages';
 import DocumentList from '../organisms/document/DocumentList';
 
-const Document: FC = (): JSX.Element => {
-  const auth = useAuth();
-  useEffect(
-    () => {
-      const asyncGet = async () => {
-        const result = await getUserDocumentList();
-        auth.setUser({ ...auth.user, documentList: result.data.document });
-      };
-      asyncGet().then().catch();
-    },
-  );
-
-  return (
-    <ControlPanelTemplate page={Pages.document}>
-      <DocumentList />
-    </ControlPanelTemplate>
-  );
-};
+const Document: FC = (): JSX.Element => (
+  <ControlPanelTemplate page={Pages.document}>
+    <DocumentList />
+  </ControlPanelTemplate>
+);
 
 export default Document;
