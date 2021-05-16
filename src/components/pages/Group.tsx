cb1600cefba214afb6981cc28b394f2be0cf67cd
sya-ri <sya79lua@gmail.com>
2021-05-16 14:12:13 +0900

    perf: APIコールをリロード時のみ行うように変更

--- src/components/pages/Group.tsx
+++ src/components/pages/Group.tsx
@@ -1,31 +1,12 @@
-import React, { useEffect } from 'react';
-import { useAuth } from '../../contexts/AuthContext';
-import { getUserGroupList } from '../../api/api';
+import React from 'react';
 import GroupList from '../organisms/group/GroupList';
 import ControlPanelTemplate from '../templates/ControlPanelTemplate';
 import { Pages } from '../../pages';
 
-const Group = (): JSX.Element => {
-  const auth = useAuth();
-  useEffect(
-    () => {
-      const asyncGet = async () => {
-        const result = await getUserGroupList();
-        auth.setUser({
-          ...auth.user,
-          groupList: result.data.group,
-        });
-      };
-      asyncGet()
-        .then()
-        .catch();
-    },
-  );
-  return (
-    <ControlPanelTemplate page={Pages.group}>
-      <GroupList />
-    </ControlPanelTemplate>
-  );
-};
+const Group = (): JSX.Element => (
+  <ControlPanelTemplate page={Pages.group}>
+    <GroupList />
+  </ControlPanelTemplate>
+);
 
 export default Group;
