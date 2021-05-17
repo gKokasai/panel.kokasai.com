48b6ebe4fcc1c0915ec19e1d2643c468f8e7b77d
sya-ri <sya79lua@gmail.com>
2021-05-18 03:40:18 +0900

    refactor: Pages.hrefに変数を付加できるようにした

--- src/components/pages/Empty.tsx
+++ src/components/pages/Empty.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ControlPanelTemplate from '../templates/ControlPanelTemplate';
-import { Pages } from '../../pages';
 
 const Empty = (): JSX.Element => (
-  <ControlPanelTemplate page={Pages.empty}>
+  <ControlPanelTemplate page={{ name: '' }}>
     <></>
   </ControlPanelTemplate>
 );
