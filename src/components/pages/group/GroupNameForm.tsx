be856f4b78dda0b5fb74adc06f9abad8cc95fa67
butsuli-shine <cheichey.1223@gmail.com>
2021-05-17 23:47:54 +0900

    feat: /group以下のページを作成した

---
+++ src/components/pages/group/GroupNameForm.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { ListItem } from '@material-ui/core';
+import { FormatAlignRight } from '@material-ui/icons';
+import Typography from '../../atoms/Typography';
+import List from '../../molecules/List';
+import { useAuth } from '../../../contexts/AuthContext';
+import ListStyle from '../../organisms/common/List.style';
+import { Pages } from '../../../pages';
+import ListItemIcon from '../../molecules/ListItemIcon';
+import ListItemText from '../../atoms/ListItemText';
+import ControlPanelTemplate from '../../templates/ControlPanelTemplate';
+import { getGroupFormList } from '../../../api/api';
+
+const GroupNameForm = (): JSX.Element => {
+  const auth = useAuth();
+  const classes = ListStyle();
+  const params: {groupName: string} = useParams();
+  useEffect(
+    () => {
+      const asyncGet = async () => {
+        const result = await getGroupFormList(params.groupName);
+        auth.setUser({
+          ...auth.user,
+          formList: result.data.form,
+        });
+      };
+      asyncGet()
+        .then()
+        .catch();
+    },
+    [],
+  );
+  return (
+    <ControlPanelTemplate page={Pages.groupNameForm}>
+      <List className={classes.list}>
+        <Typography variant="h6">
+          フォーム一覧
+        </Typography>
+        { (() => {
+          if (!auth.user?.formList || (Object.keys(auth.user.formList).length === 0)) {
+            return <>Loading</>;
+          }
+          const formList = auth.user?.formList;
+          const keys = Object.keys(formList);
+          return (
+            keys.map(
+              (formName) => (
+                <Link to={`/group/${params.groupName}/form/${formName}`}>
+                  <ListItem>
+                    <ListItemIcon>
+                      <FormatAlignRight />
+                    </ListItemIcon>
+                    <ListItemText>
+                      {formList[formName].name}
+                    </ListItemText>
+                  </ListItem>
+                </Link>
+              ),
+            )
+          );
+        })()}
+      </List>
+    </ControlPanelTemplate>
+  );
+};
+
+export default GroupNameForm;
