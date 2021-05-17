40d6588ea6341c35ea3cbd074b613460839c3057
sya-ri <sya79lua@gmail.com>
2021-05-18 03:57:11 +0900

    fix: ページリロードしても 404 エラーが出ないようにした

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -22,6 +22,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./static/index.html",
+      inject: false
     }),
     new HtmlWebpackPlugin({
       filename: "404.html",
