ebe17955685c4e493454c9f45fdedfdb480966e7
sya-ri <sya79lua@gmail.com>
2021-05-19 15:09:12 +0900

    feat: 資料がなかった場合にエラーアラートを表示するようにした

--- src/components/pages/document/Document.tsx
+++ src/components/pages/document/Document.tsx
@@ -3,13 +3,17 @@ import ControlPanelTemplate from '../../templates/ControlPanelTemplate';
 import { Pages } from '../../../pages';
 import DocumentList from '../../organisms/document/DocumentList';
 import { getDocument } from '../../../api/api';
+import { useAlert } from '../../../contexts/AlertContext';
 
 const Document: FC = (): JSX.Element => {
+  const alert = useAlert();
   const queryParameter = window.location.search;
   if (queryParameter) {
     getDocument(queryParameter.substring(1)).then((response) => {
       const url = window.URL || webkitURL;
       window.location.replace(url.createObjectURL(response.data));
+    }).catch(() => {
+      alert.error('資料の取得に失敗しました');
     });
     return <></>;
   }
