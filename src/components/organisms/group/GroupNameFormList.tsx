685ee6fd3960f4ab316479c9c642cfaea0ac46d4
sya-ri <sya79lua@gmail.com>
2021-05-20 18:58:56 +0900

    refactor: organisms に Group 関連を追加

---
+++ src/components/organisms/group/GroupNameFormList.tsx
@@ -0,0 +1,54 @@
+import React, { FC } from 'react';
+import { ListItem } from '@material-ui/core';
+import { FormatAlignRight } from '@material-ui/icons';
+import LoadableItems from '../common/LoadableItems';
+import { FormListType } from '../../../contexts/User';
+import InternalLink from '../../molecules/InternalLink';
+import { Pages } from '../../../pages';
+import ListItemIcon from '../../atoms/ListItemIcon';
+import ListItemText from '../../atoms/ListItemText';
+import WithHeaderList from '../common/WithHeaderList';
+import ListStyle from '../common/List.style';
+
+export type GroupNameFormListProps = {
+  items?: FormListType,
+  load: () => void,
+  groupName: string,
+};
+
+const GroupNameFormList: FC<GroupNameFormListProps> = (props) => {
+  const { items, load, groupName } = props;
+  const classes = ListStyle();
+  return (
+    <WithHeaderList
+      title="フォーム一覧"
+      listClassName={classes.list}
+    >
+      <LoadableItems<FormListType>
+        items={items}
+        load={load}
+        onComplete={(_items) => {
+          const keys = Object.keys(_items);
+          return (
+            keys.map(
+              (formName) => (
+                <InternalLink to={Pages.groupNameFormName.href(groupName, formName)}>
+                  <ListItem>
+                    <ListItemIcon>
+                      <FormatAlignRight />
+                    </ListItemIcon>
+                    <ListItemText>
+                      {_items[formName].name}
+                    </ListItemText>
+                  </ListItem>
+                </InternalLink>
+              ),
+            )
+          );
+        }}
+      />
+    </WithHeaderList>
+  );
+};
+
+export default GroupNameFormList;
