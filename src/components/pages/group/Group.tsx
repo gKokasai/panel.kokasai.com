a3a4816fc2935af97adcd8c4ae63487c0f251fa6
sya-ri <sya79lua@gmail.com>
2021-05-20 18:58:56 +0900

    refactor: organisms に Group 関連を追加

--- src/components/pages/group/Group.tsx
+++ src/components/pages/group/Group.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import GroupList from '../../organisms/group/GroupList';
 import ControlPanelTemplate from '../../templates/ControlPanelTemplate';
 import { Pages } from '../../../pages';
@@ -7,16 +7,14 @@ import { useAuth } from '../../../contexts/AuthContext';
 
 const Group = (): JSX.Element => {
   const auth = useAuth();
-  useEffect(
-    () => {
-      if (!auth.user?.groupList) {
-        getUserGroupList().then((response) => auth.setUser({ ...auth.user, groupList: response.data.group }));
-      }
-    }, [], // eslint-disable-line react-hooks/exhaustive-deps
-  );
   return (
     <ControlPanelTemplate page={Pages.group}>
-      <GroupList />
+      <GroupList
+        items={auth.user?.groupList}
+        load={() => {
+          getUserGroupList().then((response) => auth.setUser({ ...auth.user, groupList: response.data.group }));
+        }}
+      />
     </ControlPanelTemplate>
   );
 };
--- src/components/pages/group/GroupName.tsx
+++ src/components/pages/group/GroupName.tsx
@@ -1,45 +1,14 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { ListItem } from '@material-ui/core';
-import { FormatAlignRight, Person } from '@material-ui/icons';
 import { Pages } from '../../../pages';
 import ControlPanelTemplate from '../../templates/ControlPanelTemplate';
-import ListItemIcon from '../../atoms/ListItemIcon';
-import ListItemText from '../../atoms/ListItemText';
-import ListStyle from '../../organisms/common/List.style';
-import InternalLink from '../../molecules/InternalLink';
-import WithHeaderList from '../../organisms/common/WithHeaderList';
+import GroupNameList from '../../organisms/group/GroupNameList';
 
 const GroupName = (): JSX.Element => {
   const params: {groupName: string} = useParams();
-  const classes = ListStyle();
   return (
     <ControlPanelTemplate page={Pages.groupName}>
-      <WithHeaderList
-        title="項目一覧"
-        listClassName={classes.list}
-      >
-        <InternalLink to={Pages.groupNameForm.href(params.groupName)}>
-          <ListItem className={classes.listItem}>
-            <ListItemIcon>
-              <FormatAlignRight />
-            </ListItemIcon>
-            <ListItemText>
-              form
-            </ListItemText>
-          </ListItem>
-        </InternalLink>
-        <InternalLink to={Pages.groupNameMember.href(params.groupName)}>
-          <ListItem className={classes.listItem}>
-            <ListItemIcon>
-              <Person />
-            </ListItemIcon>
-            <ListItemText>
-              member
-            </ListItemText>
-          </ListItem>
-        </InternalLink>
-      </WithHeaderList>
+      <GroupNameList groupName={params.groupName} />
     </ControlPanelTemplate>
   );
 };
