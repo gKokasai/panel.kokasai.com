d71051d66438bbabc40afafb52d0ebad11158442
butsuli-shine <cheichey.1223@gmail.com>
2021-05-21 14:07:36 +0900

    refactor: GroupNameFormNameページをAtomic Designに則ったコンポーネント構造にした
    - GroupNameFormNameのHeaderはUserContextのformListから参照するためWithHeaderListのpropsのtitleをundefined許容した。

--- src/components/pages/group/GroupNameFormName.tsx
+++ src/components/pages/group/GroupNameFormName.tsx
@@ -1,99 +1,34 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import Checkbox from '../../atoms/Checkbox';
-import Typography from '../../atoms/Typography';
 import ControlPanelTemplate from '../../templates/ControlPanelTemplate';
 import { Pages } from '../../../pages';
 import { useAuth } from '../../../contexts/AuthContext';
 import { getGroupForm } from '../../../api/api';
-import TextField from '../../atoms/TextField';
-import Button from '../../atoms/Button';
-import LoadableItem from '../../molecules/LoadableItem';
-import { GetGroupFormResponse } from '../../../api/dataType';
 import LinearLoading from '../../molecules/LinearLoading';
+import GroupNameFormContent from '../../organisms/group/GroupNameFormContent';
 
 const GroupNameFormName = (): JSX.Element => {
   const auth = useAuth();
   const params: {groupName: string, formName: string} = useParams();
   return (
     <ControlPanelTemplate page={Pages.groupNameFormName}>
-      <Typography variant="h5">
-        {auth.user?.formList && auth.user.formList[params.formName].name}
-      </Typography>
-      <LoadableItem<GetGroupFormResponse>
-        item={auth.user?.form?.[params.formName]}
-        load={() => {
-          getGroupForm(params.groupName, params.formName).then((response) => {
-            auth.setUser({
-              ...auth.user,
-              form: {
-                ...auth.user?.form,
-                [params.formName]: response.data,
-              },
+      <GroupNameFormContent
+        load={
+          () => {
+            getGroupForm(params.groupName, params.formName).then((response) => {
+              auth.setUser({
+                ...auth.user,
+                form: {
+                  ...auth.user?.form,
+                  [params.formName]: response.data,
+                },
+              });
             });
-          });
-        }}
+          }
+        }
         LoadComponent={<LinearLoading />}
-        onComplete={(item) => (
-          <>
-            <p>
-              <Typography variant="subtitle1">{item.description}</Typography>
-            </p>
-            <p>
-              <Typography variant="caption">
-                最終期限
-                {item.limit}
-              </Typography>
-            </p>
-            <p>
-              <Typography variant="caption">
-                最終更新日時
-                {item.update}
-              </Typography>
-            </p>
-            {Object.keys(item.values).map(
-              (name) => {
-                const formDataValue = item.values[name];
-                const formDataValueType = formDataValue.type;
-                return (
-                  <>
-                    <p>
-                      <Typography>{formDataValue.name}</Typography>
-                    </p>
-                    <p>
-                      <Typography>{formDataValue.description}</Typography>
-                    </p>
-                    {
-                        (() => {
-                          if (formDataValueType[0] === 'string') {
-                            return <TextField variant="outlined" fullWidth />;
-                          }
-                          return (
-                            Object.keys(formDataValueType[1].element).map(
-                              (id) => (
-                                <p>
-                                  {formDataValueType[1].element[id as keyof string]}
-                                  <Checkbox />
-                                </p>
-                              ),
-                            )
-                          );
-                        })()
-                      }
-                  </>
-                );
-              },
-            )}
-            <Typography variant="h6">コメント</Typography>
-            <Typography variant="body1">{item.comment}</Typography>
-            <Button variant="contained" color="secondary">
-              元に戻す
-            </Button>
-            <Button variant="contained" color="primary">
-              保存する
-            </Button>
-          </>
-        )}
+        item={auth.user?.form?.[params.formName]}
+        formName={auth.user?.formList?.formName?.name}
       />
     </ControlPanelTemplate>
   );
