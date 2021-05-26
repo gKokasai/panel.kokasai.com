afc1825001cc9292f8316965b755ac8babf05fc0
sya-ri <sya79lua@gmail.com>
2021-05-26 21:40:30 +0900

    fix-ui: グループフォームのスタイルを改善した

---
+++ src/components/molecules/WithCaptionItem.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from 'react';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { Theme } from '@material-ui/core';
+import { Variant } from '@material-ui/core/styles/createTypography';
+import Typography from '../atoms/Typography';
+
+export type WithCaptionItemProps = {
+  captionVariant?: Variant;
+  caption: string;
+}
+
+const WithCaptionItemStyle = makeStyles((theme: Theme) => createStyles({
+  space: {
+    padding: `0 ${theme.spacing(2)}px`,
+  },
+}));
+
+const WithCaptionItem: FC<WithCaptionItemProps> = (props): JSX.Element => {
+  const { captionVariant, caption, children } = props;
+  const classes = WithCaptionItemStyle();
+  return (
+    <>
+      <Typography variant={captionVariant || 'caption'}>
+        {caption}
+      </Typography>
+      <div className={classes.space}>
+        {children}
+      </div>
+    </>
+  );
+};
+
+export default WithCaptionItem;
