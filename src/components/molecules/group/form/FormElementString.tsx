a40126a70213c5a48517dd46ea3ebdbdaa00b860
sya-ri <sya79lua@gmail.com>
2021-05-26 21:40:30 +0900

    fix-ui: グループフォームのスタイルを改善した

--- src/components/molecules/group/form/FormElementString.tsx
+++ src/components/molecules/group/form/FormElementString.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Theme } from '@material-ui/core';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 import TextField from '../../../atoms/TextField';
 import { FormDefineTypeString, FormSaveTypeString } from '../../../../api/dataType';
+
+const FormElementStringStyle = makeStyles((theme: Theme) => createStyles({
+  field: {
+    padding: `${theme.spacing(1)}px 0`,
+  },
+}));
 
 export type FormElementStringProps = FormDefineTypeString & FormSaveTypeString;
 
 const FormElementString = (props: FormElementStringProps): JSX.Element => {
   const { content } = props;
+  const classes = FormElementStringStyle();
   return (
-    <TextField variant="outlined" fullWidth value={content} />
+    <TextField variant="outlined" fullWidth value={content} className={classes.field} />
   );
 };
 
