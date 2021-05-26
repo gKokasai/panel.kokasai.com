4a4bb40f3c5e6a3fb5b312c3d659988ce8b7a8db
sya-ri <sya79lua@gmail.com>
2021-05-26 15:20:23 +0900

    refactor: api/dataType の定義を使うように守勢

--- src/api/dataType.ts
+++ src/api/dataType.ts
@@ -1,23 +1,78 @@
 export type FormDefineType =
-  [
-    'string',
-    {} // eslint-disable-line @typescript-eslint/ban-types
-  ] |
-  [
-    type: 'check',
-    element: {
-      [id: string]: string,
-    },
-    limit: number,
-  ];
+  ['string', FormDefineTypeString] |
+  ['check', FormDefineTypeCheck];
+
+export type FormDefineTypeString = {
+};
+
+export type FormDefineTypeCheck = {
+  element: {
+    [id: string]: string,
+  },
+  limit: number,
+};
 
 export type FormSaveType =
-  ['string', { content: string }] |
-  ['check', { select: number[] }];
+  ['string', FormSaveTypeString] |
+  ['check', FormSaveTypeCheck];
+
+export type FormSaveTypeString = {
+  content: string,
+};
+
+export type FormSaveTypeCheck = {
+  select: number[],
+};
 
 export type FormSaveValue = {
   value : FormSaveType,
   comment: string,
+};
+
+export type GetSessionResponse = {
+  count: number,
+};
+
+export type GetGroupListResponse = {
+  group: string[],
+};
+
+export type GetGroupDocumentListResponse = {
+  document: string[],
+};
+
+export type PostGroupDocumentListRequest = {
+  document: string[],
+};
+
+export type GetGroupUserListResponse = {
+  owner: string[],
+  member: string[],
+};
+
+export type PostGroupUserListRequest = {
+  owner: string[],
+  member: string[]
+};
+
+export type GetUserDocumentListResponse = {
+  document: string[],
+};
+
+export type GetUserGroupListResponse = {
+  group: string[],
+};
+
+export type FormList = {
+  [id: string]: {
+    name: string,
+    update: string,
+    status: number,
+  }
+}
+
+export type GetGroupFormListResponse = {
+  form: FormList,
 };
 
 export type GetGroupFormResponse = {
@@ -36,5 +91,10 @@ export type GetGroupFormResponse = {
   },
   status: number,
   comment: string,
-
 }
+
+export type PostGroupFormSubmitRequest = {
+  values: {
+    [id: number]: FormSaveType
+  },
+};
