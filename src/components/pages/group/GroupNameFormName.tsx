58cd5fdc5ea88a85eb643f65e3034d418e9b053f
butsuli-shine <cheichey.1223@gmail.com>
2021-06-15 16:38:40 +0900

    fix: GroupFormListを表示するようにした

--- src/components/pages/group/GroupNameFormName.tsx
+++ src/components/pages/group/GroupNameFormName.tsx
@@ -10,7 +10,7 @@ import GroupNameFormContent from "../../organisms/group/GroupNameFormContent";
 const GroupNameFormName = (): JSX.Element => {
   const auth = useAuth();
   const params: { groupName: string; formName: string } = useParams();
-
+  const { groupName, formName } = params;
   return (
     <ControlPanelTemplate page={Pages.groupNameFormName}>
       <GroupNameFormContent
@@ -18,15 +18,21 @@ const GroupNameFormName = (): JSX.Element => {
           getGroupForm(params.groupName, params.formName).then((response) => {
             auth.setUser({
               ...auth.user,
-              form: {
-                ...auth.user?.form,
-                [params.formName]: response.data,
+              group: {
+                ...auth.user?.group,
+                [groupName]: {
+                  ...auth.user?.group?.[groupName],
+                  form: {
+                    ...auth.user?.group?.[groupName].form,
+                    [formName]: response.data,
+                  },
+                },
               },
             });
           });
         }}
         LoadComponent={<LinearLoading />}
-        item={auth.user?.form?.[params.formName]}
+        item={auth.user?.group?.[groupName]?.form?.[params.formName]}
         formName={params.formName}
         groupName={params.groupName}
       />
