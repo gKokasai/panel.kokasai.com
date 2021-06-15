61eb13ba399d7b5b576d3ef0ab21d94a3f069631
butsuli-shine <cheichey.1223@gmail.com>
2021-06-15 16:38:40 +0900

    fix: フォームの値を変更できない問題を修正

--- src/components/molecules/group/form/FormElementString.tsx
+++ src/components/molecules/group/form/FormElementString.tsx
@@ -1,19 +1,14 @@
-import React from "react";
-import { Theme } from "@material-ui/core";
-import { createStyles, makeStyles } from "@material-ui/core/styles";
-import TextField from "../../../atoms/TextField";
-import {
-  FormDefineTypeString,
-  FormSaveTypeString,
-} from "../../../../api/dataType";
+import React from 'react';
+import { Theme } from '@material-ui/core';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import TextField from '../../../atoms/TextField';
+import { FormDefineTypeString, FormSaveTypeString } from '../../../../api/dataType';
 
-const FormElementStringStyle = makeStyles((theme: Theme) =>
-  createStyles({
-    field: {
-      padding: `${theme.spacing(1)}px 0`,
-    },
-  })
-);
+const FormElementStringStyle = makeStyles((theme: Theme) => createStyles({
+  field: {
+    padding: `${theme.spacing(1)}px 0`,
+  },
+}));
 
 export type FormElementStringProps = FormDefineTypeString & FormSaveTypeString;
 
@@ -21,12 +16,7 @@ const FormElementString = (props: FormElementStringProps): JSX.Element => {
   const { content } = props;
   const classes = FormElementStringStyle();
   return (
-    <TextField
-      variant="outlined"
-      fullWidth
-      defaultValue={content}
-      className={classes.field}
-    />
+    <TextField variant="outlined" fullWidth defaultValue={content} className={classes.field} />
   );
 };
 
