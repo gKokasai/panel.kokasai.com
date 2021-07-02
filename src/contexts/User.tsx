a45beb2b93e50f718ab5d1b0ab51324594f52c63
sya-ri <sya79lua@gmail.com>
2021-07-02 11:38:42 +0900

    feat: /form ページの作成

--- src/contexts/User.tsx
+++ src/contexts/User.tsx
@@ -2,6 +2,9 @@ import { FormList, GetGroupFormResponse } from "../api/dataType";
 
 export type User = {
   documentList?: string[];
+  formList?: {
+    [groupName: string]: FormList;
+  };
   groupList?: string[];
   group?: {
     [groupName: string]: {
