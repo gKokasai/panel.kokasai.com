7e3e92fc59408691d2be4e8b4ae765f3c3ba4628
cheichey <cheichey.1223@gmail.com>
2021-07-16 09:39:44 +0900

    fix-ui: PasswordRequestForm の UI を TextField の error を利用するようにした。

--- src/components/molecules/login/StudentNumberTextField.tsx
+++ src/components/molecules/login/StudentNumberTextField.tsx
@@ -1,19 +1,32 @@
 import React, { FC } from "react";
 import TextField, { TextFieldProps } from "../../atoms/TextField";
 
-export type StudentNumberTextFieldProps = TextFieldProps;
+export type StudentNumberTextFieldProps = TextFieldProps & {
+  inputId?: string;
+  enableSendButton?: boolean;
+};
 
 const StudentNumberTextField: FC<StudentNumberTextFieldProps> = (
   props
-): JSX.Element => (
-  <TextField
-    fullWidth
-    type="email"
-    label="学籍番号"
-    placeholder="アルファベットから始まる学籍番号"
-    margin="normal"
-    {...props} /* eslint-disable-line react/jsx-props-no-spreading */
-  />
-);
+): JSX.Element => {
+  const { inputId, enableSendButton } = props;
+  const displayHelperMessage = enableSendButton && !!inputId;
+  return (
+    <TextField
+      fullWidth
+      type="email"
+      label="学籍番号"
+      placeholder="アルファベットから始まる学籍番号"
+      margin="normal"
+      error={displayHelperMessage}
+      helperText={
+        displayHelperMessage
+          ? "アルファベットから始まる学籍番号を入力してください"
+          : ""
+      }
+      {...props} /* eslint-disable-line react/jsx-props-no-spreading */
+    />
+  );
+};
 
 export default StudentNumberTextField;
